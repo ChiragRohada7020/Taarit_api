f38984ab32b84594f5611b02e8619fa224a3187b
Chirag Rohada <115901486+ChiragRohada7020@users.noreply.github.com>
2023-03-19 23:44:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ class create_dict(dict):
         self[key] = value 
         
 
-@app.route("/",methods = ['POST', 'GET'])
-def Home():
+@app.route("/users",methods = ['POST', 'GET'])
+def Users():
     mycol = mydb['Users']
     
     mydict = create_dict()
@@ -32,3 +32,48 @@ def Home():
         i = i+1
     
     return jsonify(mydict)
+
+@app.route("/accident",methods = ['POST', 'GET'])
+def Accident():
+    mycol = mydb['accident']
+    
+    mydict = create_dict()
+    i = 1
+    for y in mycol.find():
+        mydict.add(i, ({"Device_id":y['iot_id'],"Location":y['location'],"Vehicle_no":y['vehicle_no'],"Email":y['email'],"Critical":y['critical'],"Postcode":y['postcode'],"Date":y['date'],"Staff_email":y['staff_email'],"Staff_name":y['staff_name'],"Rescued":y['user_rescued']}))
+        i = i+1
+    
+    return jsonify(mydict)
+
+
+
+
+
+
+@app.route("/hospital_connect",methods = ['POST', 'GET'])
+def Hospital_connect():
+    mycol = mydb['HospitalData']
+    
+    mydict = create_dict()
+    i = 1
+    for y in mycol.find():
+        mydict.add(i, ({"Name":y['name'],"City":y['city'],"Location":y['location'],"Zipcode":y['zipcode']}))
+        i = i+1
+    
+    return jsonify(mydict)
+
+
+@app.route("/Staff",methods = ['POST', 'GET'])
+def Staff():
+    mycol = mydb['Staff']
+    
+    mydict = create_dict()
+    i = 1
+    for y in mycol.find():
+        mydict.add(i, ({"Email":y['email'],"Name":y['name']}))
+        i = i+1
+    
+    return jsonify(mydict)
+
+
+

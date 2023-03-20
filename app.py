9c792de5f0dcdf493567929f9844db24c05dab10
Chirag Rohada <115901486+ChiragRohada7020@users.noreply.github.com>
2023-03-20 17:53:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ mydb = myclient["IOT"]
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
 
-class create_dict(dict): 
+# class create_dict(dict): 
   
-    # __init__ function 
-    def __init__(self): 
-        self = dict() 
+#     # __init__ function 
+#     def __init__(self): 
+#         self = dict() 
           
-    # Function to add key:value 
-    def add(self, key, value): 
-        self[key] = value 
+#     # Function to add key:value 
+#     def add(self, key, value): 
+#         self[key] = value 
         
 
 
@@ -45,11 +45,14 @@ def Users(api):
 
         mycol = mydb['Users']
     
-        mydict = create_dict()
-        i = 1
+       
+        
+        list=[]
         for y in mycol.find():
-            mydict.add(i, ({"name":y['name'],"email":y['email']}))
-            i = i+1
+            list.append({"name":y['name'],"email":y['email']})
+
+        mydict={"response":list}
+            
     
         return jsonify(mydict)
     else:
@@ -61,12 +64,12 @@ def Accident(api):
     if (Api(api)):
             mycol = mydb['accident']
     
-            mydict = create_dict()
+            list=[]
             i = 1
             for y in mycol.find():
-                mydict.add(i, ({"Device_id":y['iot_id'],"Location":y['location'],"Vehicle_no":y['vehicle_no'],"Email":y['email'],"Critical":y['critical'],"Postcode":y['postcode'],"Date":y['date'],"Staff_email":y['staff_email'],"Staff_name":y['staff_name'],"Rescued":y['user_rescued']}))
-                i = i+1
-    
+                list.append({"Device_id":y['iot_id'],"Location":y['location'],"Vehicle_no":y['vehicle_no'],"Email":y['email'],"Critical":y['critical'],"Postcode":y['postcode'],"Date":y['date'],"Staff_email":y['staff_email'],"Staff_name":y['staff_name'],"Rescued":y['user_rescued']})
+                
+            mydict={"response":list}
             return jsonify(mydict)
     else:
         return "wrong api_key"
@@ -82,12 +85,12 @@ def Hospital_connect(api):
     if (Api(api)):
         mycol = mydb['HospitalData']
     
-        mydict = create_dict()
+        list=[]
         i = 1
         for y in mycol.find():
-            mydict.add(i, ({"Name":y['name'],"City":y['city'],"Location":y['location'],"Zipcode":y['zipcode']}))
-            i = i+1
-    
+            list.append({"Name":y['name'],"City":y['city'],"Location":y['location'],"Zipcode":y['zipcode']})
+            
+        mydict={"response":list}
         return jsonify(mydict)
     else:
         return "wrong api_key"
@@ -100,11 +103,12 @@ def Staff(api):
     if (Api(api)):
             mycol = mydb['Staff']
     
-            mydict = create_dict()
+            list=[]
             i = 1
             for y in mycol.find():
-                mydict.add(i, ({"Email":y['email'],"Name":y['name']}))
-                i = i+1
+                list.append({"Email":y['email'],"Name":y['name']})
+
+            mydict={"response":list}  
     
             return jsonify(mydict)
     else:

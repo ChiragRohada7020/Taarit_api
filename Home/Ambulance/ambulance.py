71b28d052a80f4e59d431f5793c27fd0eb00b985
chirag <webholderx@gmail.com>
2023-03-21 19:47:40 +0530

    commited

--- Home/Ambulance/ambulance.py
+++ Home/Ambulance/ambulance.py
@@ -24,88 +24,112 @@ import string
 import random
 
 
+def Api(api):
+    mycol = mydb['Api']
+    
+    api1=mycol.find({"api_key":api})
+    print(api1)
+
+    
+    api3=0
+    for i in api1:
+        api3=i
+
+    return api3
 
 
 
-@ambulance.route("/ambulance_register",methods = ['POST', 'GET'])
-def ambulance_register():
+@ambulance.route("/ambulance_register/<api>",methods = ['POST', 'GET'])
+def ambulance_register(api):
     try:
+        if (Api(api)):
         
-        mobile=request.json["mobile"]
-        name=request.json["name"]
-        address=request.json["address"]
-        email=request.json["email"]
-        user_password=request.json["password"]
-        password = generate_password_hash(user_password)
-        mycol = mydb["Staff"]
-        mycol.insert_one({"name":name,"email":email,"mobile":mobile,"address":address,"password":password})
-        return "Success", 201
+            name=request.json["name"]
+            mobile=request.json["mobile"]
+            address=request.json["address"]
+            email=request.json["email"]
+            user_password=request.json["password"]
+            password = generate_password_hash(user_password)
+            mycol = mydb["Staff"]
+            mycol.insert_one({"name":name,"email":email,"mobile":mobile,"address":address,"password":password})
+            return "Success", 201
+        else:
+            return "Error",400
     except:
         return "error",400
     
-@ambulance.route("/ambulance_login",methods = ['POST', 'GET'])
-def ambulance_login():
+@ambulance.route("/ambulance_login/<api>",methods = ['POST', 'GET'])
+def ambulance_login(api):
     try:
+        if (Api(api)):
         
 
         
        
-        email=request.json["email"]
-        password=request.json["password"]
+            email=request.json["email"]
+            password=request.json["password"]
         
-        mycol = mydb["Staff"]
-        user=mycol.find_one({"email":email})
+            mycol = mydb["Staff"]
+            user=mycol.find_one({"email":email})
 
-        if (check_password_hash(user['password'], password)):
-            session['ambulance_login']=email
+            if (check_password_hash(user['password'], password)):
+                session['ambulance_login']=email
+            else:
+                return "Wrong Credentials",400
 
-        return "Success", 201
+            return "Success", 201
     except:
         return "error",400
 
 
-@ambulance.route("/ambulance_accident/<lat>/<log>",methods = ['POST', 'GET'])
-def ambulance_accident(lat,log):
+@ambulance.route("/ambulance_accident/<lat>/<log>/<api>",methods = ['POST', 'GET'])
+def ambulance_accident(lat,log,api):
     try:
+        if (Api(api)):
 
 
 
-        mycol = mydb["accident"]
-        x=mycol.find({'critical':1})
-        list=[]
-        for i in x:
-            loc1=(float(i['location'][0]),float(i['location'][1]))
-            loc2=(float(lat),float(log))
-            distance=hs.haversine(loc1,loc2)
+            mycol = mydb["accident"]
+            x=mycol.find({'critical':1})
+            list=[]
+            for i in x:
+                loc1=(float(i['location'][0]),float(i['location'][1]))
+                loc2=(float(lat),float(log))
+                distance=hs.haversine(loc1,loc2)
             
             
         
 
             
-            if(distance<5):
-                list.append({"vehicle_no":i['vehicle_no'],"location":loc1,"email":i["email"],"date":i['date'],"vehicle_id":i['iot_id']})
+                if(distance<5):
+                    list.append({"vehicle_no":i['vehicle_no'],"location":loc1,"email":i["email"],"date":i['date'],"vehicle_id":i['iot_id']})
 
         
 
-        mydict={"response":list}
+            mydict={"response":list}
             
     
-        return jsonify(mydict)
+            return jsonify(mydict)
+        else:
+            return "wrong api",400
     except:
         return "error",400
 
     
 
-@ambulance.route("/ambulance_accept",methods = ['POST', 'GET'])
-def ambulance_acept():
+@ambulance.route("/ambulance_accept/<api>",methods = ['POST', 'GET'])
+def ambulance_acept(api):
     try:
+        if (Api(api)):
 
-        iot_id=request.json["vehicle_id"]
-        driver_email=request.json["email"]
-        driver_name=request.json["name"]
-        mycol = mydb["accident"]
-        mycol.update_one({'iot_id':iot_id},{"$set":{'staff_email':driver_email,'staff_name':driver_name}})
-        return "ok"
+            iot_id=request.json["vehicle_id"]
+            driver_email=request.json["email"]
+            driver_name=request.json["name"]
+            mycol = mydb["accident"]
+            mycol.update_one({'iot_id':iot_id},{"$set":{'staff_email':driver_email,'staff_name':driver_name}})
+            return "ok"
+        else:
+            return "wrong api",400
     except:
         return "error",400
 
